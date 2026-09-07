3f5a8d8093f2034eac8b64ab4190face404e2380
Kryakikrya <gon4.egor@gmail.com>
2026-09-07 20:00:04 +0300

    Build

--- Assets/Gameplay/Scripts/Context/Factories/GameEntryLifetimeScope.cs
+++ Assets/Gameplay/Scripts/Context/Factories/GameEntryLifetimeScope.cs
@@ -9,25 +9,30 @@ public class GameEntryLifetimeScope : LifetimeScope
     [SerializeField] private Player _player;
     [SerializeField] private UIDocument _uiDocument;
 
+    private ScorePlayerData _playerData;
+
     protected override void Configure(IContainerBuilder builder)
     {
-        ScorePlayerData playerData = new ScorePlayerData();
+        _playerData = new ScorePlayerData();
 
-        var label = _uiDocument.rootVisualElement.Q<Label>("ScoreValue");
-
-        label.SetBinding("text", new DataBinding
-        {
-            dataSource = playerData,
-            dataSourcePath = new PropertyPath(nameof(ScorePlayerData.Score)),
-            bindingMode = BindingMode.ToTarget
-        });
-
-        builder.RegisterInstance(playerData);
+        builder.RegisterInstance(_playerData);
         builder.RegisterInstance(_uiDocument);
 
         builder.Register<PlayerInput>(Lifetime.Scoped);
         builder.Register<ScoreSystem>(Lifetime.Scoped);
 
         builder.RegisterInstance(_player);
+    }
+
+    private void Start()
+    {
+        var label = _uiDocument.rootVisualElement.Q<Label>("ScoreValue");
+
+        label.SetBinding("text", new DataBinding
+        {
+            dataSource = _playerData,
+            dataSourcePath = new PropertyPath(nameof(ScorePlayerData.Score)),
+            bindingMode = BindingMode.ToTarget
+        });
     }
 }
